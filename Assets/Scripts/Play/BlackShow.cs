3a110aa1c5fd777f67ae9d55eec94a7f227ac46d
SmallPig <664235822@qq.com>
2018-02-02 17:37:14 +0800

    添加选择出战卡组的功能

---
+++ Assets/Scripts/Play/BlackShow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackShow : MonoBehaviour {
+    public static BlackShow instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void Show(bool isShow)
+    {
+        gameObject.SetActive(isShow);
+    }
+}
---
+++ Assets/Scripts/Play/ChooseGroup.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Xml;
+
+public class ChooseGroup : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+        UIButton button = GetComponent<UIButton>();
+        if (!File.Exists(Constants.path))
+        {
+            button.isEnabled = false;
+            return;
+        }
+
+        XmlDocument xml = new XmlDocument();
+        xml.Load(Constants.path);
+        XmlElement root = xml.DocumentElement;
+        XmlNode group = root.SelectSingleNode(string.Format("/root/{0}", name));
+        if (group.Attributes["save"].Value == true.ToString())
+            button.isEnabled = true;
+        else
+            button.isEnabled = false;
+	}
+
+    public void OnClick()
+    {
+        BlackShow.instance.Show(false);
+        transform.parent.gameObject.SetActive(false);
+    }
+}
