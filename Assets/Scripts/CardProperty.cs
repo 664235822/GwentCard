40e735b3339dc2b8cdd8a55c538d90dc8c802b4f
SmallPig <664235822@qq.com>
2018-02-05 21:06:37 +0800

    添加卡牌属性

---
+++ Assets/Scripts/CardProperty.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class CardProperty : MonoBehaviour {
+    public Constants.Line line;
+    public Constants.Effect effect;
+    public bool gold;
+    public int power;
+    public Transform[] muster;
+}
--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -5,5 +5,7 @@ using UnityEngine;
 public class Constants : MonoBehaviour {
     public enum Group { northern, nilfgaardian, monster, scoiatael }
     public enum List { leader, special, monster, neutral }
+    public enum Line { melee, ranged, siege, empty, agile }
+    public enum Effect { empty, clear_sky, dummy, fog, frost, improve_neighbours, muster, nurse, rain, same_type_morale, scorch, spy, warhorn }
     public static readonly string path = string.Format("{0}/SaveCards.xml", Application.persistentDataPath);
 }
