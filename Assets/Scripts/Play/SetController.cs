f9bf31cc1533ef368be20e1fc1b81d2028734fb0
SmallPig <664235822@qq.com>
2018-04-08 01:04:15 +0800

    添加设置界面

---
+++ Assets/Scripts/Play/SetController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetController : Singleton<SetController> {
+    [SerializeField] GameObject set;
+    [SerializeField] UIToggle toggle;
+    [SerializeField] UIButton button;
+
+    public void Show()
+    {
+        BlackShow.GetInstance().Show(true);
+        set.SetActive(true);
+        PlayerController.GetInstance().player.SetActive(false);
+        EnemyController.GetInstance().enemy.SetActive(false);
+    }
+
+    public void Check()
+    {
+        if (toggle.value)
+            MusicController.GetInstance().Continue();
+        else
+            MusicController.GetInstance().Pause();
+    }
+
+    public void Return()
+    {
+        BlackShow.GetInstance().Show(false);
+        set.SetActive(false);
+        PlayerController.GetInstance().player.SetActive(true);
+        EnemyController.GetInstance().enemy.SetActive(true);
+    }
+}
