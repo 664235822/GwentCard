e70afc98aacd13eb98280ecd63c4d342933fb768
SmallPig <664235822@qq.com>
2018-04-11 04:32:23 +0800

    添加结束回合后life_gem的动画效果

--- Assets/Scripts/Play/GameController.cs
+++ Assets/Scripts/Play/GameController.cs
@@ -5,12 +5,11 @@ using UnityEngine;
 public class GameController : Singleton<GameController> {
     public UIAtlas[] atlas;
     public GameObject cardPerfab;
-    [SerializeField] UISprite[] player_life_gem;
-    [SerializeField] UISprite[] enemy_life_gem;
+    [SerializeField] TweenAlpha[] player_life_gem;
+    [SerializeField] TweenAlpha[] enemy_life_gem;
     bool offensive;
     int player_fail = 0;
     int enemy_fail = 0;
-    readonly Color black = new Color(0.112f, 0.255f, 0.255f, 0.255f);
 
     public void StartGame(string playerGroup)
     {
@@ -29,18 +28,18 @@ public class GameController : Singleton<GameController> {
         int power = PowerController.GetInstance().player_total - PowerController.GetInstance().enemy_total;
         if (power > 0)
         {
-            enemy_life_gem[enemy_fail].color = black;
+            enemy_life_gem[enemy_fail].PlayForward();
             enemy_fail++;
         }
         else if (power < 0)
         {
-            player_life_gem[player_fail].color = black;
+            player_life_gem[player_fail].PlayForward();
             player_fail++;
         }
         else if (power == 0)
         {
-            player_life_gem[player_fail].color = black;
-            enemy_life_gem[enemy_fail].color = black;
+            player_life_gem[player_fail].PlayForward();
+            enemy_life_gem[enemy_fail].PlayForward();
             player_fail++;
             enemy_fail++;
         }
