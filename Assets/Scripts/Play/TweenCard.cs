b0c0c7a82031c86c35e72112cd43cce13bf0238c
SmallPig <664235822@qq.com>
2018-04-16 08:35:41 +0800

    添加tweencard动画效果的初步资源

---
+++ Assets/Scripts/Play/TweenCard.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TweenCard : Singleton<TweenCard> {
+    [SerializeField] GameObject obj;
+
+    public IEnumerator Play(Transform card)
+    {
+        obj.GetComponent<UISprite>().atlas = card.GetComponent<UISprite>().atlas;
+        obj.GetComponent<UISprite>().spriteName = card.GetComponent<UISprite>().spriteName;
+        obj.transform.Find("TweenEffect").GetComponent<UISprite>().spriteName = string.Format("card_effect_{0}", card.GetComponent<CardProperty>().effect.ToString());
+        obj.GetComponent<TweenAlpha>().PlayForward();
+        yield return new WaitForSeconds(2.0f);
+        obj.transform.Find("TweenEffect").GetComponent<TweenAlpha>().PlayForward();
+        yield return new WaitForSeconds(4.0f);
+        obj.GetComponent<TweenAlpha>().PlayReverse();
+    }
+
+}
